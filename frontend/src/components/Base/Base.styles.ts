05df2e4f904e3c7a8503891e6c3763dbb7f12595
supertgo <thiagoroberto2003@gmail.com>
2024-04-13 07:48:49 -0300

    refactor: lint

--- frontend/src/components/Base/Base.styles.ts
+++ frontend/src/components/Base/Base.styles.ts
@@ -9,13 +9,12 @@ export const ContentWrapper = styled.div`
   display: flex;
 
   ${media.lessThan('large')`
-  flex-direction: column;
+    flex-direction: column;
   `}
 `;
 
 export const ChildrenWrapper = styled.div`
   width: calc(100% - 27.2rem);
-  /* width: 100%; */
   background: #fff;
 
   ${media.lessThan('large')`

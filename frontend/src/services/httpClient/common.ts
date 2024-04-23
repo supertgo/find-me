9e49f8cb1a352ca6b820a4aa2dc2c06c604f5f0b
supertgo <thiagoroberto2003@gmail.com>
2024-04-23 19:11:26 -0300

    feat: store common information about HTTP methods in one file

---
+++ frontend/src/services/httpClient/common.ts
@@ -0,0 +1,16 @@
+export enum HttpStatusCode {
+  ok = 200,
+  noContent = 204,
+  badRequest = 400,
+  unauthorized = 401,
+  notFound = 404,
+  serverError = 500,
+}
+
+export type HttpResponse = {
+  status: HttpStatusCode;
+  data?: any;
+  message?: string;
+};
+
+

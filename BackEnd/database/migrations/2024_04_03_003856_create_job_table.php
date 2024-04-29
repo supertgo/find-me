13a338dcbd31b9a4b7b1f5ab3be3967a4be67be0
Davi Araujo <portodaviporto@gmail.com>
2024-04-29 20:03:05 -0300

    create professional experience table

--- BackEnd/database/migrations/2024_04_03_003856_create_job_table.php
+++ BackEnd/database/migrations/2024_04_03_003856_create_job_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -21,8 +20,8 @@ return new class extends Migration
             $table->unsignedInteger('salary')->nullable();
             $table->enum('salary_time_unit', ['hour', 'week', 'day', 'month'])->nullable();
             $table->dateTime('accept_application_until')->nullable();
-            $table->enum('work_model', ['hybrid', 'onSite', 'homeOffice'] )->nullable();
-            $table->enum('employment_type', ['full-time', 'part-time'] )->nullable();
+            $table->enum('work_model', ['hybrid', 'onSite', 'homeOffice'])->nullable();
+            $table->enum('employment_type', ['full-time', 'part-time'])->nullable();
             $table->unsignedMediumInteger('week_workload')->nullable();
             $table->string('location')->nullable();
             $table->foreignId('company_id')->constrained('companies');

b07f88525810fee4e6f4b32e3551c68356f623fa
Davi Araujo <portodaviporto@gmail.com>
2024-04-29 18:28:34 -0300

    add academicRecords in indexUser

---
+++ BackEnd/tests/Feature/User/AcademicRecord/IndexUserWithAcademicRecordsTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tests\Feature\User\AcademicRecord;
+
+use App\Domain\User\UserIncludesEnum;
+use App\Models\AcademicRecord;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
+
+class IndexUserWithAcademicRecordsTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    const ROUTE = self::BASE_ROUTE . 'user';
+
+    public function testShowUserSuccess()
+    {
+        $users = User::factory()->count(3)->create();
+
+        $users->each(function ($user) {
+            $this->makeAcademicRecords($user);
+        });
+
+        $this
+            ->actingAs(User::factory()->create())
+            ->json(
+                'GET',
+                sprintf(self::ROUTE, $users->first()->id),
+                [
+                    'includes' => [UserIncludesEnum::AcademicRecords->value]
+                ]
+            )
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'name',
+                        'email',
+                        'phone',
+                        'id',
+                        'type',
+                        'academic_records' => [
+                            '*' => [
+                                'id',
+                                'institution',
+                                'degree',
+                                'start_date',
+                                'end_date',
+                                'field_of_study',
+                                'is_in_progress',
+                                'description',
+                                'created_at',
+                                'updated_at',
+                            ]
+                        ]
+                    ]
+                ]
+            ]);
+    }
+
+    private function makeAcademicRecords(User $user): void
+    {
+        AcademicRecord::factory()
+            ->count(3)
+            ->create(['user_id' => $user->id]);
+    }
+}

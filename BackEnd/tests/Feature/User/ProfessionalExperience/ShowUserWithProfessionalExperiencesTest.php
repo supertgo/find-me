7562378931d95edc57d3fbf95a167c00a5e325c6
Davi Araujo <portodaviporto@gmail.com>
2024-04-29 21:19:13 -0300

    add experience to show user test

---
+++ BackEnd/tests/Feature/User/ProfessionalExperience/ShowUserWithProfessionalExperiencesTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Feature\User\ProfessionalExperience;
+
+use App\Domain\User\UserIncludesEnum;
+use App\Models\ProfessionalExperience;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
+
+class ShowUserWithProfessionalExperiencesTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    const ROUTE = self::BASE_ROUTE . 'user/%s';
+
+    public function testShowUserSuccess()
+    {
+        $this->makeUser();
+        $this->makeProfessionalExperiences();
+
+        $this
+            ->actingAs(User::factory()->create())
+            ->json(
+                'GET',
+                sprintf(self::ROUTE, $this->user->id),
+                [
+                    'includes' => [UserIncludesEnum::ProfessionalExperiences->value]
+                ]
+            )
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJsonCount(3, 'data.professional_experiences')
+            ->assertJsonStructure([
+                'data' => [
+                    'name',
+                    'email',
+                    'phone',
+                    'id',
+                    'type',
+                    'professional_experiences' => [
+                        '*' => [
+                            'id',
+                            'company_name',
+                            'position',
+                            'start_date',
+                            'end_date',
+                            'is_current',
+                            'location',
+                            'work_model',
+                            'employment_type',
+                            'description',
+                            'created_at',
+                            'updated_at',
+                        ]
+                    ]
+                ]
+            ]);
+    }
+
+    private function makeProfessionalExperiences(): void
+    {
+        ProfessionalExperience::factory()
+            ->count(3)
+            ->create(['user_id' => $this->user->id]);
+    }
+}

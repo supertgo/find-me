4c244b71c8af094af3191c0e4cd60a6ceb76355b
supertgo <thiagoroberto2003@gmail.com>
2024-04-30 19:02:10 -0300

    feat: call use jobs into Job template

--- frontend/src/templates/Jobs/Jobs.spec.tsx
+++ frontend/src/templates/Jobs/Jobs.spec.tsx
@@ -1,16 +1,15 @@
 import 'components/Input/Input.mock';
 import 'components/JobFilter/JobFilter.mock';
-import 'components/JobItem/JobItem.mock';
 import { render, screen } from 'utils/test/test-utils';
 import { Jobs } from './Jobs';
 
 describe('<Jobs />', () => {
   it('should render the component', () => {
-    render(<Jobs />, {
+    const { container } = render(<Jobs />, {
       queryProvider: true,
     });
 
+    expect(container.getElementsByClassName('sc-blmEgr sc-dJGMql ddGRwL kDPwos').length).toBe(4);
     expect(screen.getByTestId('Mock JobFilter')).toBeInTheDocument();
-    expect(screen.getAllByTestId('Mock JobItem')).toHaveLength(4);
   });
 });

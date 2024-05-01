431ba2f14c0edb3ff3a47c0e05585b8207ac18c7
supertgo <thiagoroberto2003@gmail.com>
2024-05-01 19:02:51 -0300

    feat: add show user and find user routes consts

--- frontend/src/utils/routes.ts
+++ frontend/src/utils/routes.ts
@@ -1,3 +1,13 @@
-export const PostAuthLoginRouteConst = 'auth/login'
+export type GetUserRouteConstProps = {
+  user_id: string;
+};
 
-export const PostAuthLogOutRouteConst = 'auth/logout'
+export const PostAuthLoginRouteConst = 'auth/login';
+
+export const PostAuthLogOutRouteConst = 'auth/logout';
+
+export const GetUsersRouteConst = 'user';
+
+export const GetUserRouteConst = ({ user_id }: GetUserRouteConstProps) => {
+  return `user/${user_id}`;
+};

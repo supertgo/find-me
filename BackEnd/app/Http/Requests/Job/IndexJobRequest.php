0ccda4a4291b8141e1331526cc2a0c7bd6a4744d
Davi Araujo <portodaviporto@gmail.com>
2024-05-02 16:43:45 -0300

    test include de company no index job

---
+++ BackEnd/app/Http/Requests/Job/IndexJobRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests\Job;
+
+use App\Domain\Job\Enum\JobIncludesEnum;
+use App\Http\Requests\AbstractRequest;
+
+class IndexJobRequest extends AbstractRequest
+{
+    protected array $availableIncludes = [
+        JobIncludesEnum::Company->value
+    ];
+
+    public function rules(): array
+    {
+        return [
+            'includes' => 'array',
+            'includes.*' => 'string|in:' . implode(',', $this->availableIncludes),
+        ];
+    }
+
+
+}

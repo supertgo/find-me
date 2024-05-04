aa6263e5c4880f938d63f34bdf92e8ba4a797d84
supertgo <thiagoroberto2003@gmail.com>
2024-05-04 17:36:01 -0300

    feat: ch GetUserRouteConst to handle with includes parameters

--- frontend/src/utils/routes.ts
+++ frontend/src/utils/routes.ts
@@ -1,5 +1,8 @@
+import { UserIncludeOption } from 'protocols/external/user/user';
+
 export type GetUserRouteConstProps = {
   user_id: number;
+  includes?: UserIncludeOption[];
 };
 
 export const PostAuthLoginRouteConst = 'auth/login';
@@ -8,6 +11,16 @@ export const PostAuthLogOutRouteConst = 'auth/logout';
 
 export const GetUsersRouteConst = 'user';
 
-export const GetUserRouteConst = ({ user_id }: GetUserRouteConstProps) => {
-  return `user/${user_id}`;
+export const GetUserRouteConst = ({
+  user_id,
+  includes,
+}: GetUserRouteConstProps) => {
+  let userRoute = `user/${user_id}`;
+
+  if (includes && includes.length) {
+    userRoute += `?includes[]=`;
+    userRoute += includes.join('&includes[]=');
+  }
+
+  return userRoute;
 };

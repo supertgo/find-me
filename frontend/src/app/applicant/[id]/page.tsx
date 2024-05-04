2efbd4a8a593bc7baa076f9848a98e347935d61c
supertgo <thiagoroberto2003@gmail.com>
2024-05-04 17:42:41 -0300

    feat: add next auth applicant page

---
+++ frontend/src/app/applicant/[id]/page.tsx
@@ -0,0 +1,42 @@
+import { nextAuthOptions } from 'app/api/auth/[...nextauth]/options';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+import { UserProps } from 'protocols/external/user/user';
+import { Applicant } from 'templates/Applicant/Applicant';
+import { GetUserRouteConst } from 'utils/routes';
+
+type GetDataProps = {
+  data: UserProps;
+};
+
+type ApplicantPageProps = {
+  params: {
+    id: number;
+  };
+};
+
+async function getUserData(user_id: number) {
+  const token = await getServerSession(nextAuthOptions);
+
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/${GetUserRouteConst({
+      user_id,
+      includes: ['competences', 'academicRecords', 'professionalExperiences'],
+    })}`,
+    {
+      headers: { Authorization: `Bearer ${token?.access_token}` },
+    },
+  );
+
+  if (!res.ok) {
+    return redirect('/home');
+  }
+
+  return res.json();
+}
+
+export default async function ConfigPage({ params }: ApplicantPageProps) {
+  const { data }: GetDataProps = await getUserData(params.id);
+
+  return <Applicant user={data} />;
+}

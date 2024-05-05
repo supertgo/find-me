f4cae1d54b55194e6a1655fc63452af4911e587b
supertgo <thiagoroberto2003@gmail.com>
2024-05-05 08:19:27 -0300

    test: applicant

--- frontend/src/templates/Applicant/Applicant.spec.tsx
+++ frontend/src/templates/Applicant/Applicant.spec.tsx
@@ -1,12 +1,26 @@
-import { render, screen } from 'utils/test/test-utils'
-import { Applicant } from './Applicant'
+import 'components/Sidebar/Sidebar.mock';
+import { render, screen } from 'utils/test/test-utils';
+import { Applicant } from './Applicant';
+import { completeUserMock } from 'test/mocks/external/users';
 
 describe('<Applicant />', () => {
   it('should render the component', () => {
-    render(
-      <Applicant />
-    )
+    render(<Applicant user={completeUserMock} />);
 
-    expect(screen.getByText('Applicant')).toBeInTheDocument()
-  })
-})
+    expect(screen.getByText('Detalhes do Candidato')).toBeInTheDocument();
+    expect(screen.getByText('E-mail')).toBeInTheDocument();
+    expect(screen.getByText(completeUserMock.email)).toBeInTheDocument();
+    expect(screen.getByText('Sobre mim')).toBeInTheDocument();
+    expect(screen.getByText(completeUserMock.about_me!)).toBeInTheDocument();
+    expect(screen.getByText('Emprego Atual')).toBeInTheDocument();
+    expect(screen.getByText('Desempregado')).toBeInTheDocument();
+    expect(screen.getByText('Qualificação Acadêmica')).toBeInTheDocument();
+    expect(
+      screen.getByText(completeUserMock.academic_records![0].institution),
+    ).toBeInTheDocument();
+    expect(screen.getByText('Área de Estudo')).toBeInTheDocument();
+    expect(
+      screen.getByText(completeUserMock.academic_records![0].field_of_study),
+    ).toBeInTheDocument();
+  });
+});

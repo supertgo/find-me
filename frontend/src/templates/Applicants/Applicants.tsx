c43e1bfc46d97e994476ddf13961809158345199
supertgo <thiagoroberto2003@gmail.com>
2024-05-05 06:04:05 -0300

    feat: use globalFilter

--- frontend/src/templates/Applicants/Applicants.tsx
+++ frontend/src/templates/Applicants/Applicants.tsx
@@ -1,20 +1,26 @@
 import { Base } from 'components/Base/Base';
-import { TableData } from 'components/Table/TableData/TableData';
-import { Title } from 'components/Title/Title';
 import { Button } from 'components/Button/Button';
 import { DropboxIcon } from 'icons/DropboxIcon/DropboxIcon';
 import { useApplicantsTable } from 'hooks/useApplicantsTable/useApplicantsTable';
+import { useEffect } from 'react';
 
 import * as S from './Applicants.styles';
-import { Pagination } from 'components/Pagination/Pagination';
+import { ApplicantsTable } from 'components/ApplicantsTable/ApplicantsTable';
 
 export const Applicants = () => {
-  const { data, table, isLoading } = useApplicantsTable();
+  const { data, table, isLoading, globalFilter, setGlobalFilter } =
+    useApplicantsTable();
 
   const applicantsData = data?.data.data;
 
   const currentPage = table.getState().pagination.pageIndex + 1;
   const itemsPerPage = 10;
+
+  useEffect(() => {
+    return () => {
+      setGlobalFilter('');
+    };
+  }, []);
 
   return (
     <Base>
@@ -30,32 +36,15 @@ export const Applicants = () => {
           <Button>Publicar uma vaga</Button>
         </S.Header>
 
-        <Title
-          title={`Total de Candidatos: ${applicantsData?.length || '...'}`}
-        />
-
-        <TableData
-          data={table}
+        <ApplicantsTable
+          table={table}
+          applicantsData={applicantsData}
           isLoading={isLoading}
-          defaultMessage="Não há nada para essa configurações"
+          globalFilter={globalFilter}
+          setGlobalFilter={setGlobalFilter}
+          currentPage={currentPage}
+          itemsPerPage={itemsPerPage}
         />
-
-        {applicantsData && (
-          <S.PaginationWrapper>
-            <Pagination
-              hasNextPage={table.getCanNextPage()}
-              hasPrevPage={table.getCanPreviousPage()}
-              page={currentPage}
-              limit={itemsPerPage}
-              totalItems={applicantsData.length || 0}
-              totalPages={table.getPageCount() || 0}
-              onPageChange={table.setPageIndex}
-              goNextPage={table.nextPage}
-              goPreviousPage={table.previousPage}
-              hasDetails={true}
-            />
-          </S.PaginationWrapper>
-        )}
       </S.Wrapper>
     </Base>
   );

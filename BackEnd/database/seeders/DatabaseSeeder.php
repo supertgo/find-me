90cc5180717324ef6d4072f379287bab624cf478
supertgo <thiagoroberto2003@gmail.com>
2024-05-06 19:02:49 -0300

    feat: improve db seed

--- BackEnd/database/seeders/DatabaseSeeder.php
+++ BackEnd/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\AcademicRecord;
 use App\Models\Company;
+use App\Models\Competence;
 use App\Models\Job;
+use App\Models\ProfessionalExperience;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -16,7 +19,28 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-       $users =  User::factory(15)->create();
+        $users = User::factory(15)->create();
+
+        ProfessionalExperience::factory()->count($users->count())->create()->each(function ($professionalExperience) use ($users) {
+            $user = $users->random();
+            $user->professionalExperiences()->save($professionalExperience);
+        });
+
+        $competences = Competence::factory()->count(10)->create();
+
+        $users->each(function ($user) use ($competences) {
+            $user->competences()->attach($competences->random(rand(3, 7))->pluck('id')->toArray());
+        });
+
+        $academicRecords = AcademicRecord::factory()->count($users->count())->create();
+
+        $shuffledUsers = $users->shuffle();
+        $shuffledAcademicRecords = $academicRecords->shuffle();
+
+        $shuffledUsers->each(function ($user) use (&$shuffledAcademicRecords) {
+            $academicRecord = $shuffledAcademicRecords->pop();
+            $user->academicRecords()->save($academicRecord);
+        });
 
         Company::factory(5)->create()->each(function ($company) use ($users){
             Job::factory(3)->create([

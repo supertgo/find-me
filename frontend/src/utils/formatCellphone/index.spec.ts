0ea03d38b253dedf479bfed8edcf460d85a86ac5
analuisamessias <analuisa.mendes1712@gmail.com>
2024-05-07 13:51:49 -0300

    create formatCellphone function

---
+++ frontend/src/utils/formatCellphone/index.spec.ts
@@ -0,0 +1,16 @@
+import { formatCellphone } from ".";
+
+describe('formatCellphone()', () => {
+    it('should receive a string of 10 digits and convert into phone number', () => {
+      expect(formatCellphone('1133333333')).toBe('(11) 3333-3333')
+    })
+    it('should receive a number of 10 digits and convert into phone number', () => {
+      expect(formatCellphone(1133333333)).toBe('(11) 3333-3333')
+    })
+    it('should receive a string of 11 digits and convert into phone number', () => {
+        expect(formatCellphone('11933333333')).toBe('(11) 93333-3333')
+      })
+      it('should receive a number of 11 digits and convert into phone number', () => {
+        expect(formatCellphone(11933333333)).toBe('(11) 93333-3333')
+      })
+  })
---
+++ frontend/src/utils/formatCellphone/index.ts
@@ -0,0 +1,18 @@
+export const formatCellphone = (phone: string | number) => {
+  if (phone === null || phone === undefined) {
+    return '';
+  }
+  const formattedPhone =
+        typeof phone == 'string' ? phone.replace(/\D/g, '') : phone.toString();
+  if (formattedPhone.length !== 10 && formattedPhone.length !== 11) {
+    return '';
+  }
+  let result;
+  if (formattedPhone.length === 10) {
+    result = `(${formattedPhone.slice(0, 2)}) ${formattedPhone.slice(2, 6)}-${formattedPhone.slice(6)}`;
+  } else {
+    result = `(${formattedPhone.slice(0, 2)}) ${formattedPhone.slice(2, 3)}${formattedPhone.slice(3, 7)}-${formattedPhone.slice(7)}`;
+  }
+
+  return result;
+};

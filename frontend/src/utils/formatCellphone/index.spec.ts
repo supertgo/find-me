0875c703cefe8ff0f32c07649bb9824cfa213145
analuisamessias <analuisa.mendes1712@gmail.com>
2024-05-08 16:46:46 -0300

    create formatCellphone function

--- frontend/src/utils/formatCellphone/index.spec.ts
+++ frontend/src/utils/formatCellphone/index.spec.ts
@@ -9,8 +9,8 @@ describe('formatCellphone()', () => {
     })
     it('should receive a string of 11 digits and convert into phone number', () => {
         expect(formatCellphone('11933333333')).toBe('(11) 93333-3333')
-      })
-      it('should receive a number of 11 digits and convert into phone number', () => {
+    })
+    it('should receive a number of 11 digits and convert into phone number', () => {
         expect(formatCellphone(11933333333)).toBe('(11) 93333-3333')
-      })
+    })
   })

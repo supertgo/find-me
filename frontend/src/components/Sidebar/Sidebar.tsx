205502154aa19fe0a5f612788670dad9d3e5a59f
supertgo <thiagoroberto2003@gmail.com>
2024-05-16 14:17:25 -0300

    feat: use sidebar items based on user type

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,12 +1,13 @@
-import { SidebarItem } from 'components/SidebarItem/SidebarItem';
-import * as S from './Sidebar.styles';
-import { HomeIcon } from 'icons/HomeIcon/HomeIcon';
 import { useLoggedUserStore } from 'stores/loggedUserStore/loggedUserStore';
 import { useSignOut } from 'hooks/useSignOut/useSignOut';
 import Link from 'next/link';
-import { ApplicantsUrl, ConfigUrl, HomeUrl, JobsUrl } from 'utils/urls';
+import { ConfigUrl } from 'utils/urls';
 import { ExitIcon } from '@radix-ui/react-icons';
 import { theme } from 'styles/theme';
+import { RecruiterSidebarItems } from 'components/RecruiterSidebarItems/RecruiterSidebarItems';
+import { ApplicantSidebarItems } from 'components/ApplicantSidebarItems/ApplicantSidebarItems';
+
+import * as S from './Sidebar.styles';
 
 export type SidebarProps = {};
 
@@ -23,37 +24,9 @@ export const Sidebar = ({}: SidebarProps) => {
 			{/* Colocar componente de logo aqui */}
 			<S.Items>
 				{type === 'recruiter' ? (
-					<>
-						<SidebarItem
-							href={`/${HomeUrl}`}
-							icon={<HomeIcon />}
-							text="Início"
-						/>
-						<SidebarItem
-							href={`/${ApplicantsUrl}`}
-							icon={<HomeIcon />}
-							text="Candidatos"
-						/>
-						<SidebarItem
-							href={`/${JobsUrl}`}
-							icon={<HomeIcon />}
-							text="Vagas"
-						/>
-					</>
+					<RecruiterSidebarItems />
 				) : (
-					<>
-						<SidebarItem
-							href={`/${ApplicantsUrl}`}
-							icon={<HomeIcon />}
-							text="Candidatos"
-						/>
-						<SidebarItem href="/" icon={<HomeIcon />} text="Candidaturas" />
-						<SidebarItem
-							href={`/${JobsUrl}`}
-							icon={<HomeIcon />}
-							text="Vagas"
-						/>
-					</>
+					<ApplicantSidebarItems />
 				)}
 			</S.Items>
 

306621b9cec7b68d03357c114517df52979dfa6c
analuisamessias <analuisa.mendes1712@gmail.com>
2024-05-17 20:07:34 -0300

    refactor: change color variant

--- frontend/src/components/Pill/Pill.styles.ts
+++ frontend/src/components/Pill/Pill.styles.ts
@@ -7,25 +7,21 @@ export type WrapperProps = {
 const wrapperModifiers = {
 	success: (theme: DefaultTheme) => css`
 		color: ${theme.colors.lightGreen};
-		background: #fff;
 		border: 1px solid ${theme.colors.lightGreen};
 	`,
 
 	error: (theme: DefaultTheme) => css`
 		color: ${theme.colors.lightRed};
-		background: #fff;
 		border: 1px solid ${theme.colors.lightRed};
 	`,
 
 	info: (theme: DefaultTheme) => css`
 		color: ${theme.colors.primary};
-		background: #fff;
 		border: 1px solid ${theme.colors.primary};
 	`,
 
 	warning: (theme: DefaultTheme) => css`
 		color: ${theme.colors.mustYellow};
-		background: #fff;
 		border: 1px solid ${theme.colors.mustYellow};
 	`,
 };
@@ -34,7 +30,7 @@ export const Wrapper = styled.div<WrapperProps>`
 	${({ theme, $variant }) => css`
 		padding: ${theme.borderRadius.s} 1rem;
 		border-radius: 8rem;
-		background: #56cdad1a;
+		background: ${theme.colors.white};
 		color: ${theme.colors.lightGreen};
 		font-size: ${theme.font.sizes.xs};
 		font-weight: ${theme.font.weights.medium};

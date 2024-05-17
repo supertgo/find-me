ddfd6fe2ecef39810e1f76d95388646bdcc0745d
supertgo <thiagoroberto2003@gmail.com>
2024-05-17 18:17:17 -0300

    feat: add Dialog

---
+++ frontend/src/components/Dialog/Dialog.stories.tsx
@@ -0,0 +1,50 @@
+import type { Meta, StoryFn } from '@storybook/react';
+import { useState } from 'react';
+
+import { Dialog } from './Dialog';
+import { theme } from 'styles/theme';
+import { Button } from 'components/Button/Button';
+import { Input } from 'components/Input/Input';
+
+export default {
+	title: 'components/Dialog',
+	component: Dialog.Root,
+	tags: ['autodocs'],
+} as Meta;
+
+export const Default: StoryFn = () => {
+	return (
+		<Dialog.Root open={true} onOpenChange={() => ({})}>
+			<Dialog.Trigger asChild>
+				<Button>Open Dialog</Button>
+			</Dialog.Trigger>
+
+			<Dialog.Portal>
+				<Dialog.Overlay>
+					<Dialog.Content size="large">
+						<Dialog.Header>
+							<Dialog.Title>Adicionar Formação Acadêmica</Dialog.Title>
+						</Dialog.Header>
+
+						<Dialog.Description>
+              <Input placeholder="Nome da empresa" />
+              <Input placeholder="Nome da empresa" />
+              <Input placeholder="Nome da empresa" />
+              <Input placeholder="Nome da empresa" />
+              <Input placeholder="Nome da empresa" />
+						</Dialog.Description>
+
+						<Dialog.Footer>
+							<Dialog.Close asChild>
+								<Button>Cancelar</Button>
+							</Dialog.Close>
+							<Dialog.Close asChild>
+								<Button>Salvar</Button>
+							</Dialog.Close>
+						</Dialog.Footer>
+					</Dialog.Content>
+				</Dialog.Overlay>
+			</Dialog.Portal>
+		</Dialog.Root>
+	);
+};

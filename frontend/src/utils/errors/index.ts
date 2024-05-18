9eea08137b69c6f03ae51ce74b7dcceb197fb96a
supertgo <thiagoroberto2003@gmail.com>
2024-05-18 16:12:15 -0300

    feat: add new errors const's

--- frontend/src/utils/errors/index.ts
+++ frontend/src/utils/errors/index.ts
@@ -8,18 +8,6 @@ export const REQUIRED_PASSWORD = "A senha é obrigatória.";
 
 export const REQUIRED_COMPETENCE = "A competência é obrigatória.";
 
-export const REQUIRED_ACADEMIC_RECORD_INSTITUTION = "A instituição é obrigatória.";
-
-export const REQUIRED_ACADEMIC_RECORD_DEGREE = "O degree é obrigatório.";
-
-export const REQUIRED_ACADEMIC_RECORD_FIELD_STUDY = "O campo de estudo é obrigatório.";
-
-export const REQUIRED_ACADEMIC_RECORD_START_DATE = "A data de início é obrigatória.";
-
-export const REQUIRED_ACADEMIC_RECORD_END_DATE = "A data de término é obrigatória.";
-
-export const REQUIRED_ACADEMIC_RECORD_DESCRIPTION = "A descrição é obrigatória.";
-
 export const PASSWORD_CONFIRMATION_REQUIRED =
   "A confirmação da senha é obrigatória.";
 
@@ -33,3 +21,35 @@ export const UNEXPECTED_ERROR = "Algo deu errado. Tente mais tarde.";
 export const REQUIRED_CELLPHONE = "O celular é obrigatório";
 
 export const REQUIRED_USER = "O usuário é obrigatório";
+
+
+
+
+
+export const REQUIRED_ACADEMIC_RECORD_INSTITUTION = "A instituição é obrigatória.";
+
+export const REQUIRED_ACADEMIC_RECORD_DEGREE = "O degree é obrigatório.";
+
+export const REQUIRED_ACADEMIC_RECORD_FIELD_STUDY = "O campo de estudo é obrigatório.";
+
+export const REQUIRED_ACADEMIC_RECORD_START_DATE = "A data de início é obrigatória.";
+
+export const REQUIRED_ACADEMIC_RECORD_END_DATE = "A data de término é obrigatória.";
+
+export const REQUIRED_ACADEMIC_RECORD_DESCRIPTION = "A descrição é obrigatória.";
+
+
+
+
+export const REQUIRED_PROFESSIONAL_EXPERIENCE_COMPANY = 'O nome da empresa é obrigatório.'
+
+export const REQUIRED_PROFESSIONAL_EXPERIENCE_POSITION = 'O cargo é obrigatório.'
+
+export const REQUIRED_PROFESSIONAL_EXPERIENCE_DESCRIPTION = 'A descrição é obrigatória.'
+
+export const REQUIRED_PROFESSIONAL_EXPERIENCE_LOCATION = 'A localização obrigatória.'
+
+export const REQUIRED_PROFESSIONAL_EXPERIENCE_START_DATE = 'A data de início é obrigatória.'
+
+export const REQUIRED_PROFESSIONAL_EXPERIENCE = 'O nome da empresa é obrigatório.'
+

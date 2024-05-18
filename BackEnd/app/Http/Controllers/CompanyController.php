2eb563fb7adb8762313b874a47d240d8b99d3208
Davi Araujo <portodaviporto@gmail.com>
2024-05-18 06:45:46 -0300

    add company controller

---
+++ BackEnd/app/Http/Controllers/CompanyController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Domain\Company\CompanyServiceInterface;
+use App\Exceptions\Abstract\AbstractFindMeException;
+use App\Http\Requests\Company\StoreCompanyRequest;
+use App\Http\Resources\CompanyResource;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Log;
+use Symfony\Component\HttpFoundation\Response;
+
+class CompanyController
+{
+    public function store(StoreCompanyRequest $request): JsonResponse
+    {
+        try {
+            $company = app(CompanyServiceInterface::class)
+                ->create($request->getLoggedUserId(), $request->validated());
+
+            return CompanyResource::make($company)
+                ->toResponse($request)
+                ->setStatusCode(Response::HTTP_CREATED);
+        } catch (AbstractFindMeException $exception) {
+            return response()->json(
+                $exception->render(),
+                status: $exception->getCode()
+            );
+
+        } catch (Exception $exception) {
+            Log::error($exception);
+
+            return response()
+                ->json(
+                    ['error' => 'Server error'],
+                    Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}

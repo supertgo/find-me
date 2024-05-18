855e1cc2e60b9f0c1f75509efe1724ed22933e45
supertgo <thiagoroberto2003@gmail.com>
2024-05-18 16:19:50 -0300

    chore: rm supressHydrationWarning

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -6,30 +6,35 @@ import { QueryProvider } from 'providers/query-provider';
 import NextTopLoader from 'nextjs-toploader';
 
 const epilogue = Epilogue({
-  weight: ['100', '400', '500', '600', '700', '800'],
-  subsets: ['latin'],
+	weight: ['100', '400', '500', '600', '700', '800'],
+	subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: 'FindMe',
-  description: '',
+	title: 'FindMe',
+	description: '',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="pt-br" translate="no" className={epilogue.className}>
-      <body>
-        <StyledComponentsRegistry>
-          <QueryProvider>
-            <NextTopLoader />
-            <AppProviders>{children}</AppProviders>
-          </QueryProvider>
-        </StyledComponentsRegistry>
-      </body>
-    </html>
-  );
+	return (
+		<html
+			lang="pt-br"
+			translate="no"
+			className={epilogue.className}
+			suppressHydrationWarning={true}
+		>
+			<body>
+				<StyledComponentsRegistry>
+					<QueryProvider>
+						<NextTopLoader />
+						<AppProviders>{children}</AppProviders>
+					</QueryProvider>
+				</StyledComponentsRegistry>
+			</body>
+		</html>
+	);
 }

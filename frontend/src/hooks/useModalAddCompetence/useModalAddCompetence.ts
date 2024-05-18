e829cab40798575ac5ce00d794048576f4345191
supertgo <thiagoroberto2003@gmail.com>
2024-05-18 16:15:08 -0300

    feat: reset form and call revalidate on success

--- frontend/src/hooks/useModalAddCompetence/useModalAddCompetence.ts
+++ frontend/src/hooks/useModalAddCompetence/useModalAddCompetence.ts
@@ -1,13 +1,24 @@
+import { useQueryClient } from '@tanstack/react-query';
 import { useCompetence } from 'hooks/useCompetence/useCompetence';
 import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { GetUserRouteConst } from 'utils/routes';
 
 export type AddCompetenceInputs = {
 	competence: string;
 };
 
-export const useModalAddCompetence = () => {
+export type UseModalAddCompetenceProps = {
+	user_id: number;
+};
+
+export const useModalAddCompetence = ({
+	user_id,
+}: UseModalAddCompetenceProps) => {
 	const [isLoading, setIsLoading] = useState(false);
+	const [open, setOpen] = useState(false);
+
+	const queryClient = useQueryClient();
 
 	const {
 		handleSubmit,
@@ -28,10 +39,21 @@ export const useModalAddCompetence = () => {
 			competence: data.competence,
 		});
 
+		await queryClient.invalidateQueries({
+			queryKey: [
+				`/${GetUserRouteConst({
+					user_id,
+				})}`,
+			],
+		});
+
+		setOpen(false);
 		setIsLoading(false);
 	};
 
 	return {
+		open,
+		setOpen,
 		onSubmit,
 		control,
 		handleSubmit,

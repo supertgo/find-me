51892f6c797fcf9611f11d8ba6d488f960c6a79e
supertgo <thiagoroberto2003@gmail.com>
2024-05-18 16:11:50 -0300

    feat: show error using toast

--- frontend/src/hooks/useAcademicRecord/useAcademicRecord.ts
+++ frontend/src/hooks/useAcademicRecord/useAcademicRecord.ts
@@ -1,11 +1,14 @@
-import { PostAcademicRecordBody } from 'protocols/external/academic-record/academic-record';
+import {
+	PostAcademicRecordsBody,
+	PostAcademicRecordObj,
+} from 'protocols/external/academic-record/academic-record';
 import { toast } from 'react-toastify';
 import { PostClient } from 'services/httpClient/post';
 import { UNEXPECTED_ERROR } from 'utils/errors';
 import { PostUserAcademicRecordRouteConst } from 'utils/routes';
 
 export type CreateAcademicRecordProps = {
-	academicRecord: PostAcademicRecordBody;
+	academicRecord: PostAcademicRecordObj;
 };
 
 export const useAcademicRecord = () => {
@@ -14,8 +17,8 @@ export const useAcademicRecord = () => {
 	}: CreateAcademicRecordProps) => {
 		const postClient = new PostClient();
 
-		const body: PostAcademicRecordBody = {
-			...academicRecord,
+		const body: PostAcademicRecordsBody = {
+			academic_records: [academicRecord],
 		};
 
 		try {
@@ -25,8 +28,14 @@ export const useAcademicRecord = () => {
 			});
 
 			toast.success('Formação acadêmica criada com sucesso!');
-		} catch (e) {
+		} catch (error) {
+			if (error instanceof Error) {
+				toast.error(error.response.data.message);
+				return { error: error.response.data.message };
+			}
+
 			toast.error(UNEXPECTED_ERROR);
+			return { error: UNEXPECTED_ERROR };
 		}
 	};
 

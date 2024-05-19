a446fcda32705828cdf5604bc2760adfbca8c2d0
Davi Araujo <portodaviporto@gmail.com>
2024-05-19 16:06:30 -0300

    create resumes migration

---
+++ BackEnd/database/migrations/2024_05_19_185612_create_resumes.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Domain\Resume\ResumeTypeEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('resumes', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('owner_id')->constrained('users');
+            $table->string('alias');
+            $table->enum('type', [
+                ResumeTypeEnum::File->value,
+                ResumeTypeEnum::Form->value,
+            ]);
+
+            $table->string('file_path')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('resumes');
+    }
+};

67211d2b13db87e10fb8e747c0a19913a87e82f1
Davi Araujo <portodaviporto@gmail.com>
2024-05-19 08:16:33 -0300

    adjustments-only-api always json

--- BackEnd/app/Http/Middleware/UserIsRecruiter.php
+++ BackEnd/app/Http/Middleware/UserIsRecruiter.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 
 use App\Domain\User\UserTypeEnum;
+use App\Exceptions\Auth\OnlyForRecruitersException;
 use App\Models\User;
 use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
@@ -16,20 +17,22 @@ class UserIsRecruiter extends Middleware
     /**
      * Handle an incoming request.
      *
-     * @param  Request  $request
-     * @param  Closure  $next
-     * @param  string[]  ...$guards
+     * @param Request $request
+     * @param Closure $next
+     * @param string[] ...$guards
      * @return mixed
      *
+     * @throws OnlyForRecruitersException
      */
-    public function handle($request, Closure $next, ...$guards)
+    public function handle($request, Closure $next, ...$guards): mixed
     {
         /** @var User $user */
         $user = Auth::user();
 
         if (!Auth::check() || $user->type != UserTypeEnum::Recruiter->value) {
             // If the user is not of the expected type, redirect to the login page
-            return redirect('login');
+            throw new OnlyForRecruitersException();
+
         }
 
         return $next($request);
--- BackEnd/public/index.php
+++ BackEnd/public/index.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Requests\AbstractRequest;
 use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
@@ -49,7 +49,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 $kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
-    $request = Request::capture()
+    $request = AbstractRequest::capture()
 )->send();
 
 $kernel->terminate($request, $response);

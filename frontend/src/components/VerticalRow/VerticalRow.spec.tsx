ab693073179abe4583df37a416de1f001d6edcdc
supertgo <thiagoroberto2003@gmail.com>
2024-05-20 07:41:51 -0300

    feat: add VerticalRow

---
+++ frontend/src/components/VerticalRow/VerticalRow.spec.tsx
@@ -0,0 +1,9 @@
+import { render } from 'utils/test/test-utils';
+import { VerticalRow } from './VerticalRow';
+
+describe('<VerticalRow />', () => {
+	it('should render the component', () => {
+		const { container } = render(<VerticalRow />);
+		expect(container.firstChild).toMatchInlineSnapshot();
+	});
+});

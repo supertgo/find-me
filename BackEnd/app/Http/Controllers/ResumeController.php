65dd1a012d3ae69b974da16e71a134ccfbd00da8
Davi Araujo <portodaviporto@gmail.com>
2024-05-20 19:41:45 -0300

    add patch method in controller

--- BackEnd/app/Http/Controllers/ResumeController.php
+++ BackEnd/app/Http/Controllers/ResumeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Domain\Resume\ResumeServiceInterface;
 use App\Exceptions\Abstract\AbstractFindMeException;
 use App\Http\Requests\Resume\CreateResumeRequest;
+use App\Http\Requests\Resume\ResumeRequestHavingId;
 use App\Http\Resources\ResumeResource;
 use Exception;
 use Illuminate\Http\JsonResponse;
@@ -26,6 +27,33 @@ class ResumeController extends Controller
             return ResumeResource::make($resume)
                 ->toResponse($request)
                 ->setStatusCode(Response::HTTP_CREATED);
+        } catch (AbstractFindMeException  $exception) {
+            return response()->json(
+                $exception->render(),
+                status: $exception->getHttpCode()
+            );
+        } catch (Exception $exception) {
+            Log::error($exception);
+
+            return response()
+                ->json(
+                    ['error' => 'Server error'],
+                    Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function patchAlias(ResumeRequestHavingId $request): Response
+    {
+        try {
+            $resume = app(ResumeServiceInterface::class)
+                ->patchAlias(
+                    $request->validated(),
+                    $request->getLoggedUserId()
+                );
+
+            return ResumeResource::make($resume)
+                ->toResponse($request)
+                ->setStatusCode(Response::HTTP_OK);
         } catch (AbstractFindMeException  $exception) {
             return response()->json(
                 $exception->render(),

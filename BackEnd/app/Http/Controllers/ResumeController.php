1cefa8eb5171bb502627d26223fa4a9cabda1d8a
Davi Araujo <portodaviporto@gmail.com>
2024-05-20 20:57:42 -0300

    add logic to download resume in controller

--- BackEnd/app/Http/Controllers/ResumeController.php
+++ BackEnd/app/Http/Controllers/ResumeController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Domain\Resume\ResumeServiceInterface;
 use App\Exceptions\Abstract\AbstractFindMeException;
+use App\Helpers\File\FileHelperInterface;
 use App\Http\Requests\Resume\CreateResumeRequest;
+use App\Http\Requests\Resume\DownloadResumeRequest;
 use App\Http\Requests\Resume\PatchResumeStatusRequest;
 use App\Http\Resources\ResumeResource;
 use Exception;
@@ -55,6 +57,32 @@ class ResumeController extends Controller
             return ResumeResource::make($resume)
                 ->toResponse($request)
                 ->setStatusCode(Response::HTTP_OK);
+        } catch (AbstractFindMeException $exception) {
+            return response()->json(
+                $exception->render(),
+                status: $exception->getHttpCode()
+            );
+        } catch (Exception $exception) {
+            Log::error($exception);
+
+            return response()
+                ->json(
+                    ['error' => 'Server error'],
+                    Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public function download(DownloadResumeRequest $request): Response
+    {
+        try {
+            $path = app(ResumeServiceInterface::class)
+                ->getFilePath(
+                    $request->getResumeId(),
+                    $request->getLoggedUserId()
+                );
+
+            return app(FileHelperInterface::class)
+                ->downloadPrivateFile($path);
         } catch (AbstractFindMeException $exception) {
             return response()->json(
                 $exception->render(),

350bc5e1d8e057c38c054fca8a288388186709a4
supertgo <thiagoroberto2003@gmail.com>
2024-05-21 13:00:20 -0300

    feat: show location only if its exists

--- frontend/src/components/JobItem/JobItem.tsx
+++ frontend/src/components/JobItem/JobItem.tsx
@@ -10,62 +10,64 @@ import * as S from './JobItem.styles';
 export type JobItemProps = {} & Job;
 
 export const JobItem = ({
-  name,
-  is_available,
-  applications_amount,
-  salary,
-  salary_time_unit,
-  employment_type,
-  work_model,
-  location,
+	name,
+	is_available,
+	applications_amount,
+	salary,
+	salary_time_unit,
+	employment_type,
+	work_model,
+	location,
 }: JobItemProps) => {
-  const applicants = Math.floor(Math.random() * applications_amount);
+	const applicants = Math.floor(Math.random() * applications_amount);
 
-  const jobPillProps: JobPillProps = {
-    employment_type,
-    work_model,
-    salary,
-    salary_time_unit,
-  };
+	const jobPillProps: JobPillProps = {
+		employment_type,
+		work_model,
+		salary,
+		salary_time_unit,
+	};
 
-  return (
-    <S.Wrapper>
-      <S.JobInfoWrapper>
-        <Image
-          src={`https://source.unsplash.com/random/?company_logo&${applicants}`}
-          width="64"
-          height="64"
-          alt={`company avatar`}
-          style={{
-            borderRadius: '50%',
-          }}
-          loading="lazy"
-          quality={100}
-        />
-        <S.JobInfo>
-          <p>{name}</p>
-          <S.JobLocationInfo>{filterJobLocation(location)}</S.JobLocationInfo>
-          <MediaMatch $greaterThan="large">
-            <S.PillWrapper>
-              <JobPill {...jobPillProps} />
-            </S.PillWrapper>
-          </MediaMatch>
-        </S.JobInfo>
-      </S.JobInfoWrapper>
-      <S.PillSmallScreen>
-        <JobPill {...jobPillProps} />
-      </S.PillSmallScreen>
-      <S.JobApplicationInfo>
-        <Button disabled={!is_available}>Aplicar</Button>
-        <S.ProgressWrapper>
-          <S.ProgressDiv $progress={(applicants / applications_amount) * 100} />
-        </S.ProgressWrapper>
-        <S.JobMaxInfo>
-          <p>{`${applicants} aplicados`}</p>
-          <p>{`-`}</p>
-          <p>{`Máximo ${applications_amount}`}</p>
-        </S.JobMaxInfo>
-      </S.JobApplicationInfo>
-    </S.Wrapper>
-  );
+	return (
+		<S.Wrapper>
+			<S.JobInfoWrapper>
+				<Image
+					src={`https://source.unsplash.com/random/?company_logo&${applicants}`}
+					width="64"
+					height="64"
+					alt={`company avatar`}
+					style={{
+						borderRadius: '50%',
+					}}
+					loading="lazy"
+					quality={100}
+				/>
+				<S.JobInfo>
+					<p>{name}</p>
+					{!!location && (
+						<S.JobLocationInfo>{filterJobLocation(location)}</S.JobLocationInfo>
+					)}
+					<MediaMatch $greaterThan="large">
+						<S.PillWrapper>
+							<JobPill {...jobPillProps} />
+						</S.PillWrapper>
+					</MediaMatch>
+				</S.JobInfo>
+			</S.JobInfoWrapper>
+			<S.PillSmallScreen>
+				<JobPill {...jobPillProps} />
+			</S.PillSmallScreen>
+			<S.JobApplicationInfo>
+				<Button disabled={!is_available}>Aplicar</Button>
+				<S.ProgressWrapper>
+					<S.ProgressDiv $progress={(applicants / applications_amount) * 100} />
+				</S.ProgressWrapper>
+				<S.JobMaxInfo>
+					<p>{`${applicants} aplicados`}</p>
+					<p>{`-`}</p>
+					<p>{`Máximo ${applications_amount}`}</p>
+				</S.JobMaxInfo>
+			</S.JobApplicationInfo>
+		</S.Wrapper>
+	);
 };

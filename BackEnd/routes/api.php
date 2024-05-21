17eec6066f3419cc3068cb8ea5f1986cfe420415
Davi Araujo <portodaviporto@gmail.com>
2024-05-20 22:00:54 -0300

    add index resume route

--- BackEnd/routes/api.php
+++ BackEnd/routes/api.php
@@ -56,7 +56,7 @@ Route::group(
         });
 
         Route::resource('/resume', ResumeController::class)
-            ->only(['store', 'show'])
+            ->only(['store', 'show', 'index'])
             ->middleware('auth:api');
 
         Route::group(['prefix' => '/resume'], function () {

db35b8d14532f91e3d81750f6d2abb53021d34c3
supertgo <thiagoroberto2003@gmail.com>
2024-05-21 13:03:08 -0300

    feat: add deleteProfessionalExperience

--- frontend/src/hooks/useProfessionalExperience/useProfessionalExperience.ts
+++ frontend/src/hooks/useProfessionalExperience/useProfessionalExperience.ts
@@ -1,16 +1,25 @@
 import {
+	DeleteProfessionalExperiencesBody,
 	PostAddProfessionalExperienceObj,
 	PostAddProfessionalExperiencesBody,
 } from 'protocols/external/professional-experience/professional-experience';
 import { toast } from 'react-toastify';
+import { DeleteClient } from 'services/httpClient/delete';
 import { PostClient } from 'services/httpClient/post';
 import { UNEXPECTED_ERROR } from 'utils/errors';
-import { PostUserProfessionalExperiencesRouteConst } from 'utils/routes';
+import {
+	DeleteUserAcademicRecordRouteConst,
+	PostUserProfessionalExperiencesRouteConst,
+} from 'utils/routes';
 
 export type UseProfessionalExperienceProps = {};
 
 export type CreateProfessionalExperienceProps = {
 	professionalExperience: PostAddProfessionalExperienceObj;
+};
+
+export type DeleteProfessionalExperienceProps = {
+	id: number;
 };
 
 export const useProfessionalExperience = () => {
@@ -45,7 +54,35 @@ export const useProfessionalExperience = () => {
 		}
 	};
 
+	const deleteProfessionalExperience = async ({
+		id,
+	}: DeleteProfessionalExperienceProps) => {
+		const deleteClient = new DeleteClient();
+
+		const body: DeleteProfessionalExperiencesBody = {
+			professional_experiences_id: [id],
+		};
+
+		try {
+			await deleteClient.delete({
+				url: `/${DeleteUserAcademicRecordRouteConst}`,
+				body,
+			});
+
+			toast.success('Experiência removida com sucesso!');
+		} catch (error) {
+			if (error instanceof Error) {
+				toast.error(error.response.data.message);
+				return { error: error.response.data.message };
+			}
+
+			toast.error(UNEXPECTED_ERROR);
+			return { error: UNEXPECTED_ERROR };
+		}
+	};
+
 	return {
 		createProfessionalExperience,
+		deleteProfessionalExperience,
 	};
 };

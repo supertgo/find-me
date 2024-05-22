be3ac24c10aafa5d0a14e388bd326ca808eae759
supertgo <thiagoroberto2003@gmail.com>
2024-05-22 18:59:35 -0300

    feat: create Info component

---
+++ frontend/src/components/Info/Info.spec.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from 'utils/test/test-utils'
+import { Info, InfoProps } from './Info'
+
+const props: InfoProps = {
+  title: 'Salário',
+  text: 'R$ 5892,00'
+}
+
+describe('<Info />', () => {
+  it('should render the component', () => {
+    render(
+      <Info {...props} />
+    )
+
+    expect(screen.getByText(props.title)).toBeInTheDocument()
+    expect(screen.getByText(props.text)).toBeInTheDocument()
+  })
+})

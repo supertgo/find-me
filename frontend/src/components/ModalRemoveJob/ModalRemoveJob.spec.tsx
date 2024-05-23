202474d1de08e4ebf31f6e82fc181e0684c5f258
supertgo <thiagoroberto2003@gmail.com>
2024-05-23 14:54:25 -0300

    feat(Modal): add remove job modal

---
+++ frontend/src/components/ModalRemoveJob/ModalRemoveJob.spec.tsx
@@ -0,0 +1,23 @@
+import { render, renderHook, screen } from 'utils/test/test-utils';
+import { vi } from 'vitest'
+import { ModalRemoveJob } from './ModalRemoveJob';
+import { useContextSelector } from 'use-context-selector';
+
+describe('<ModalRemoveJob />', () => {
+	it('should render the component', () => {
+		const job = {
+			name: 'Dev Front Junior III',
+			id: 106,
+			companyName: 'EmCash',
+		};
+
+		render(<ModalRemoveJob open job={job} setOpen={vi.fn()} />, {
+			queryProvider: true,
+		});
+
+		expect(screen.getByText('Dev Front Junior III')).toBeInTheDocument();
+		expect(
+			screen.getByText('Tem certeza que deseja excluir essa vaga?'),
+		).toBeInTheDocument();
+	});
+});

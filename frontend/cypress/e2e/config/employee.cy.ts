6b9b0a1a82553f050bf83334100a13097e083941
supertgo <thiagoroberto2003@gmail.com>
2024-05-25 10:38:47 -0300

    feat(cy): add professinal xp showing all the forms errors

--- frontend/cypress/e2e/config/employee.cy.ts
+++ frontend/cypress/e2e/config/employee.cy.ts
@@ -95,5 +95,49 @@ describe('Config - Employee', () => {
 
 			cy.findAllByText('AWS Engineer');
 		});
+
+		it('should be able to add an xp without end_date by sad path', () => {
+			cy.findByTitle('Adicionar Experiência').click();
+
+      cy.getByName('position').focus()
+
+      cy.findByText('O nome da empresa é obrigatório.')
+      
+      cy.getByName('location').focus()
+      cy.findByText('O cargo é obrigatório.')
+      
+      cy.getByName('start_date').focus()
+      cy.findByText('A localização é obrigatória.')
+
+      cy.findByRole('checkbox', { name: /Trabalho atualmente neste cargo/i }).click()
+
+      cy.getByName('description', 'textarea').focus()
+      cy.findByText('A data de início é obrigatória.')
+
+      cy.getByName('company_name').focus()
+      cy.findByText('A descrição é obrigatória.')
+
+			cy.getByName('company_name').type('Sad Path Job');
+
+			cy.getByName('position').type('Software Developer Sad Path');
+
+			cy.getByName('location').type('Vale do Silício');
+
+			cy.getByDataCy('work_model').select('Presencial');
+
+			cy.getByDataCy('employment_type').select('Meio Período');
+
+			cy.getByName('start_date').type('2022-05-23');
+
+			cy.getByName('description', 'textarea').type(
+				'Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet.',
+			);
+
+			cy.findByRole('button', { name: /Salvar/i }).click({
+				force: true,
+			});
+
+			cy.findAllByText('Software Developer Sad Path')
+		});
 	});
 });

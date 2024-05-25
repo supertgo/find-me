f42a49ded3f9d422cb864bc35971107837502d01
Davi Araujo <portodaviporto@gmail.com>
2024-05-25 13:37:39 -0300

    add filters to user index request

--- BackEnd/app/Http/Requests/User/IndexUserRequest.php
+++ BackEnd/app/Http/Requests/User/IndexUserRequest.php
@@ -3,7 +3,9 @@
 namespace App\Http\Requests\User;
 
 use App\Domain\User\UserIncludesEnum;
+use App\Domain\User\UserTypeEnum;
 use App\Http\Requests\AbstractRequest;
+use App\Http\Requests\Rules\UniqueArrayValuesRule;
 
 class IndexUserRequest extends AbstractRequest
 {
@@ -14,6 +16,15 @@ class IndexUserRequest extends AbstractRequest
         return [
             'includes' => 'array',
             'includes.*' => 'string|in:' . implode(',', $this->availableIncludes),
+
+            'filters' => 'array',
+
+            'filters.name' => 'string',
+            'filters.email' => 'email',
+            'filters.type' => 'string|in:' . UserTypeEnum::valuesAsString(),
+
+            'filters.competences_id' => ['array', new UniqueArrayValuesRule],
+            'filters.competences_id.*' => 'integer',
         ];
     }
 }

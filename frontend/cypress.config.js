0c09e312c69918ffc3390087c0c7d78dd668bd7e
supertgo <thiagoroberto2003@gmail.com>
2024-05-27 19:57:34 -0300

    chore(cy): increase cypress time to prevent errors

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   video: false,
-  defaultCommandTimeout: 4000,
+  defaultCommandTimeout: 8000,
   e2e: {
     failOnStatusCode: false,
     baseUrl: 'http://localhost:3000',

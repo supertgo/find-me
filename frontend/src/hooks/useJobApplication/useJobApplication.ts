04c7deabd76f27d646aea2fac388818f49176473
supertgo <thiagoroberto2003@gmail.com>
2024-05-27 12:24:42 -0300

    feat(job app): add findJobApplications fn

--- frontend/src/hooks/useJobApplication/useJobApplication.ts
+++ frontend/src/hooks/useJobApplication/useJobApplication.ts
@@ -1,21 +1,30 @@
+import { AxiosResponse } from 'axios';
 import { PostJobApplicationBody } from 'protocols/external/job/application';
+import { JobApplicationResponse } from 'protocols/external/job/job-application';
 import { toast } from 'react-toastify';
+import { GetClient } from 'services/httpClient/get';
 import { PostClient } from 'services/httpClient/post';
 import { UNEXPECTED_ERROR } from 'utils/errors';
-import { PostJobApplicationRouteConst } from 'utils/routes';
+import {
+	GetJobApplicationsRouteConst,
+	GetJobApplicationsRouteConstProps,
+	PostJobApplicationRouteConst,
+} from 'utils/routes';
 
 export type UseJobApplicationProps = {};
 
-export type CreateJobApplication = {
+export type CreateJobApplicationProps = {
 	job_id: number;
 	cover_letter: string;
 };
+
+export type FindJobApplicationProps = GetJobApplicationsRouteConstProps;
 
 export const useJobApplication = () => {
 	const createJobApplication = async ({
 		job_id,
 		cover_letter,
-	}: CreateJobApplication) => {
+	}: CreateJobApplicationProps) => {
 		const postClient = new PostClient();
 
 		const body: PostJobApplicationBody = {
@@ -40,7 +49,35 @@ export const useJobApplication = () => {
 		}
 	};
 
+	const findJobApplications = async ({
+		jobsId,
+		candidatesId,
+		includes,
+	}: FindJobApplicationProps) => {
+		const getClient = new GetClient();
+
+		try {
+			return await getClient.get<AxiosResponse<JobApplicationResponse>>({
+				url: `/${GetJobApplicationsRouteConst({
+					includes,
+					candidatesId,
+					jobsId,
+				})}`,
+			});
+
+		} catch (error) {
+			if (error instanceof Error) {
+				toast.error(error.response.data.message);
+				return { error: error.response.data.message };
+			}
+
+			toast.error(UNEXPECTED_ERROR);
+			return { error: UNEXPECTED_ERROR };
+		}
+	};
+
 	return {
 		createJobApplication,
+		findJobApplications,
 	};
 };

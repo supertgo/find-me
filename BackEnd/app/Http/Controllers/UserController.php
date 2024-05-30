19107b88ce4bee013a435994fb2309888f4b5224
Davi Araujo <portodaviporto@gmail.com>
2024-05-29 21:48:41 -0300

    update user controller to use user service with dependency injection

--- BackEnd/app/Http/Controllers/UserController.php
+++ BackEnd/app/Http/Controllers/UserController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Domain\User\UserDomain;
-use App\Domain\User\UserRepository;
-use App\Domain\User\UserService;
+use App\Domain\User\UserServiceInterface;
 use App\Exceptions\Abstract\AbstractFindMeException;
 use App\Http\Requests\AbstractRequest;
 use App\Http\Requests\User\IndexUserRequest;
@@ -16,14 +14,15 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response as IluminateResponse;
 use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
+use Throwable;
 
 class UserController extends Controller
 {
     public function index(IndexUserRequest $request): JsonResponse|IluminateResponse
     {
-        $service = new UserDomain(app(UserRepository::class));
-
         try {
+            $service = app(UserServiceInterface::class);
+
             return response()
                 ->json([
                     'data' => $service->usersWithIncludes(
@@ -31,6 +30,11 @@ class UserController extends Controller
                         $request->getIncludes()
                     )
                 ]);
+        } catch (AbstractFindMeException  $exception) {
+            return response()->json(
+                $exception->render(),
+                status: $exception->getHttpCode()
+            );
         } catch (Exception $exception) {
             Log::error($exception);
 
@@ -45,9 +49,14 @@ class UserController extends Controller
     {
         try {
             return response()->json([
-                'data' => (new UserService())
+                'data' => app(UserServiceInterface::class)
                     ->getUser($request->getUserId(), $request->getIncludes())
             ]);
+        } catch (AbstractFindMeException  $exception) {
+            return response()->json(
+                $exception->render(),
+                status: $exception->getHttpCode()
+            );
         } catch (Exception $exception) {
             Log::error($exception);
 
@@ -61,7 +70,7 @@ class UserController extends Controller
     public function update(UpdateUserRequest $request): JsonResponse|IluminateResponse
     {
         try {
-            $user = (new UserService())
+            $user = app(UserServiceInterface::class)
                 ->update(
                     ['id' => $request->getLoggedUserId()] + $request->validated()
                 );
@@ -69,8 +78,7 @@ class UserController extends Controller
             return response()->json([
                 'data' => $user
             ]);
-        } catch (AbstractFindMeException  $exception) {
-
+        } catch (AbstractFindMeException $exception) {
             return response()->json(
                 $exception->render(),
                 status: $exception->getHttpCode()
@@ -86,7 +94,7 @@ class UserController extends Controller
     public function updateProfilePicture(UpdateUserProfilePictureRequest $request): IluminateResponse|JsonResponse
     {
         try {
-            $url = (new UserService())
+            $url = app(UserServiceInterface::class)
                 ->updateProfilePicture(
                     $request->getLoggedUserId(), $request->file('profile_picture')
                 );
@@ -94,7 +102,14 @@ class UserController extends Controller
             return response()->json([
                 'url' => $url
             ]);
-        } catch (Exception) {
+        } catch (AbstractFindMeException  $exception) {
+            return response()->json(
+                $exception->render(),
+                status: $exception->getHttpCode()
+            );
+        } catch (Throwable $exception) {
+            Log::error($exception);
+
             return response()
                 ->json(
                     ['error' => 'Server error'],
@@ -105,11 +120,18 @@ class UserController extends Controller
     public function deleteProfilePicture(AbstractRequest $request): IluminateResponse|JsonResponse
     {
         try {
-            (new UserService())
+            app(UserServiceInterface::class)
                 ->deleteProfilePicture($request->getLoggedUserId());
 
             return response()->noContent();
-        } catch (Exception) {
+        } catch (AbstractFindMeException $exception) {
+            return response()->json(
+                $exception->render(),
+                status: $exception->getHttpCode()
+            );
+        } catch (Throwable $exception) {
+            Log::error($exception);
+
             return response()
                 ->json(
                     ['error' => 'Server error'],

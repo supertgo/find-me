52766bf4794d45c2d09aaa267eb6c1bef062e489
analuisamessias <analuisa.mendes1712@gmail.com>
2024-06-06 15:33:01 -0300

    refactor: corrected case test on Textarea

--- frontend/src/components/Textarea/Textarea.spec.tsx
+++ frontend/src/components/Textarea/Textarea.spec.tsx
@@ -1,10 +1,19 @@
 import { render, screen } from 'utils/test/test-utils';
 import { Textarea } from './Textarea';
+import { MaxLength } from 'utils/maxLengths';
 
 describe('<Textarea />', () => {
 	it('should render the component', () => {
 		render(<Textarea defaultValue="test" />);
 
 		expect(screen.getByRole('textbox')).toBeInTheDocument();
+	});
+	it('should render additional information when maxLength is provided', () => {
+		const props = {
+			maxLength: 10,
+		};
+		render(<Textarea defaultValue="test" {...props} />);
+
+		expect(screen.getByText(`0/${MaxLength.description}`)).toBeInTheDocument();
 	});
 });

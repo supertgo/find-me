b8f347f134943b0e7af4a240e97c27d8cd3cc460
analuisamessias <analuisa.mendes1712@gmail.com>
2024-07-08 13:25:52 -0300

    refactor: change test time from 8s to 10s

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,10 +1,10 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  video: false,
-  defaultCommandTimeout: 8000,
-  e2e: {
-    failOnStatusCode: false,
-    baseUrl: 'http://localhost:3000',
-  },
-})
+	video: false,
+	defaultCommandTimeout: 10000,
+	e2e: {
+		failOnStatusCode: false,
+		baseUrl: 'http://localhost:3000',
+	},
+});

984a742270f33b4bed2ad8e230dd368346308054
supertgo <thiagoroberto2003@gmail.com>
2024-09-07 18:25:00 -0300

    feat: mock delete user/academic-record

--- frontend/src/test/next-auth-mock.ts
+++ frontend/src/test/next-auth-mock.ts
@@ -1,5 +1,6 @@
 import { http, HttpResponse } from 'msw';
 import { setupServer } from 'msw/node';
+import { DeleteAcademicRecordBody } from 'protocols/external/academic-record/academic-record';
 
 export const ApiAuthSessionResponseMock = {
 	name: 'tobias',
@@ -8,9 +9,30 @@ export const ApiAuthSessionResponseMock = {
 	access_token: 'token',
 };
 
+const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost/api';
+
 export const handlers = [
 	http.get('/api/auth/session', () => {
 		return HttpResponse.json(ApiAuthSessionResponseMock);
+	}),
+	http.delete(`${baseUrl}/user/academic-records`, async ({ request }) => {
+		const res = (await request.json()) as DeleteAcademicRecordBody;
+		const academicRecordId = res.academic_records_id[0];
+
+		if (!academicRecordId) {
+			return HttpResponse.json(
+				{
+					message: 'Mock Error',
+				},
+				{
+					status: 404,
+				},
+			);
+		}
+
+		return HttpResponse.json({
+			ok: true,
+		});
 	}),
 ];
 

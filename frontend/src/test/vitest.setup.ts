9294b7886152d2c699cca5b9e9c4ada5fe59156b
supertgo <thiagoroberto2003@gmail.com>
2024-09-07 18:24:14 -0300

    test: send env vars from .env.test to tests

--- frontend/src/test/vitest.setup.ts
+++ frontend/src/test/vitest.setup.ts
@@ -5,3 +5,8 @@ import 'test/next-auth-mock';
 import 'test/next-navigation-mock';
 import 'test/match-media-mock';
 import 'jest-styled-components';
+import dotenv from 'dotenv';
+
+dotenv.config({
+  path: '.env.test'
+})
